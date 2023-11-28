1fd9b3434764b6fb10917036630cd885924a6da5
Luis Restrepo <luchoposada555@gmail.com>
2023-11-27 21:19:12 -0500

    Creacion de firestore y peticion

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { loginGoogle } from '../../store/users/userActions'
-
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loginGoogle } from "../../store/users/userActions";
+import { firestore } from "../../firebase/firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 
 const Login = () => {
-const dispatch = useDispatch()
-  const handleLogin = ()=>{
-     dispatch(loginGoogle())
-  } 
+  const productCollection = collection(firestore, "Productos");
+  const dispatch = useDispatch();
+  const handleLogin = () => {
+    dispatch(loginGoogle());
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await getDocs(productCollection);
+      const tempArr = [];
+      response.forEach((doc) => {
+        tempArr.push({ id: doc.id, ...doc.data() });
+      });
+      console.log(tempArr);
+    };
+    getData();
+  }, []);
+
   return (
     <div>
-      <button onClick={()=> handleLogin()}>Entrar con google</button>
+      <button onClick={() => handleLogin()}>Entrar con google</button>
     </div>
+  );
+};
 
-  )
-}
-
-export default Login
+export default Login;
